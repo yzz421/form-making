b08bd6fe1d5ab99c828de388a85d6c85d5695f42
liyongzhi <yzz.ice@gmail.com>
2020-09-04 10:58:32 +0800

    fix import file case bug

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from './index.css';
 
 const BasicLayout: React.FC = props => {
   return <div>{props.children}</div>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './layouts/layout';
+import Layout from './layouts/Layout';
 import Board from './layouts/Board';
 import WidgeList from './layouts/WidgeList';
 import { makerContext, Maker } from '@/store/Maker';
@@ -7,7 +7,6 @@ import 'antd/dist/antd.css';
 import ConfigBlock from './layouts/FieldConfig';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
-import { getGUID } from '@/utils/utils';
 
 export default function() {
   const store = React.useRef(new Maker()).current;
