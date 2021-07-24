1bba0b0dbca5243518b4931aa8fbb5ecb1d140a9
liyongzhi <yzz.ice@gmail.com>
2021-07-24 15:25:22 +0800

    修改文本

--- src/pages/Renderer/engine/Chart.tsx
+++ src/pages/Renderer/engine/Chart.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Chart: React.FC = (props: any) => (
   <div style={{ minHeight: '100px', width: '100%', border: '1px solid red' }}>
     <h5>
-      {`this is page container`}
+      {`this is chart container`}
       <br />
       {`type: "${props.layout.componentType}", name: "${props.layout.componentName}", id: "${props.layout.id}"`}{' '}
       {}
